893ba9b24a0e94506531077768b8755c17aa510d
ghadaSaeed <ghadasaeedmohammed@gmail.com>
2025-07-22 19:16:56 +0300

    Add files via upload

---
+++ hos.js
@@ -0,0 +1,28 @@
+function show() {
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'flex'; 
+  }
+  function hide() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'; 
+  }
+  function home()
+  {
+    window.location.href="index.html";
+  }
+  function about()
+  {
+    window.location.href="file:///C:/hospital%20project/heart/about.html";
+  }
+  function phar()
+  {
+    window.location.href="file:///C:/hospital%20project/phar/pharmacy.html";
+  }
+  function dep()
+  {
+    window.location.href="file:///C:/hospital%20project/index.html#title";
+  }
+  function ser()
+  {
+    window.location.href="file:///C:/hospital%20project/index.html#ser";
+  }
